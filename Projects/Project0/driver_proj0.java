0c4189a8b31ee312035b9461d7905e33987b142b
lflorez1 <laura.florezraigosa1@marist.edu>
2016-09-05 20:14:17 -0400

    This is my submission for Project0 due 09/06/16

---
+++ Projects/Project0/driver_proj0.java
@@ -0,0 +1,17 @@
+/**
+ * file: driver_proj0.java
+ * author: Laura Florez
+ * course: CMPT 220
+ * assignment: project 0
+ * due date: Sept 06/16
+ * version: 1.0
+ * 
+ * This file contains the code for project0
+ */
+
+public class driver_proj0 {
+  public static void main(String[] args) {
+    // Display message Hello World! on the console
+    System.out.println("Hello World!");  
+  }
+}
