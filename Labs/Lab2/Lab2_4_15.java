56c4a8ded9b7e4255ea754010d77a848a69f25ac
lflorez1 <laura.florezraigosa1@marist.edu>
2016-09-20 20:03:43 -0400

    I changed the style

--- Labs/Lab2/Lab2_4_15.java
+++ Labs/Lab2/Lab2_4_15.java
@@ -1,41 +1,42 @@
 /**
- * file: Lab2_4_15.java
- * author: Laura Florez
- * course: CMPT 220
- * assignment: Lab2
- * due date: Sept 14/16
- * version: 1.0
- * 
- * The key concepts of this lab is the use of selections, mathematical functions, characters and strings.
- */
+* file: Lab2_4_15.java
+* author: Laura Florez
+* course: CMPT 220
+* assignment: Lab2
+* due date: Sept 14/16
+* version: 1.0
+* 
+* The key concepts of this lab is the use of selections, 
+* mathematical functions, characters and strings.
+*/
 
 import java.util.Scanner;
 
 public class Lab2_4_15 {
   public static void main(String[] args) {
-	  Scanner input = new Scanner(System.in);
+    Scanner input = new Scanner(System.in);
     System.out.print("Enter a letter: ");
     String letter = input.nextLine();
 
     char ch = Character.toUpperCase(letter.charAt(0));
     if (ch <='C' && ch >= 'A'){
       System.out.println("The corresponding number is 2");
-        } else if (ch <= 'F' && ch >= 'D'){
-         System.out.println("The corresponding number us 3");
-         } else if (ch <= 'I' && ch >= 'G'){
-       	   System.out.println("The corresponding number is 4");
-           } else if (ch <= 'L' && ch >= 'J'){
-             System.out.println("The corresponding number is 5");
-             } else if (ch <= 'O' && ch >= 'M'){
-           	   System.out.println("The corresponding number is 6");
-               } else if (ch <= 'S' && ch >= 'P'){
-                 System.out.println("The corresponding number is 7");	
-                 } else if (ch <= 'V' && ch >= 'T'){
-               	   System.out.println("The corresponding number is 8");
-                   } else if (ch <= 'Z' && ch >= 'W'){
-                     System.out.println("The corresponding number is 9");
-                     } else {
-                   	   System.out.println("The input is invalid");
-                       }
-	}
+    } else if (ch <= 'F' && ch >= 'D'){
+      System.out.println("The corresponding number us 3");
+    } else if (ch <= 'I' && ch >= 'G'){
+       System.out.println("The corresponding number is 4");
+    } else if (ch <= 'L' && ch >= 'J'){
+       System.out.println("The corresponding number is 5");
+    } else if (ch <= 'O' && ch >= 'M'){
+       System.out.println("The corresponding number is 6");
+    } else if (ch <= 'S' && ch >= 'P'){
+       System.out.println("The corresponding number is 7");
+    } else if (ch <= 'V' && ch >= 'T'){
+       System.out.println("The corresponding number is 8");
+    } else if (ch <= 'Z' && ch >= 'W'){
+       System.out.println("The corresponding number is 9");
+    } else {
+       System.out.println("The input is invalid");
+    }
+  }
 }
