5713617fec6d4fc4cae85e225ebb61ae1313bc96
lflorez1 <laura.florezraigosa1@marist.edu>
2016-09-20 20:04:26 -0400

    I changed the style

--- Labs/Lab2/Lab2_4_5.java
+++ Labs/Lab2/Lab2_4_5.java
@@ -1,27 +1,29 @@
 /**
- * file: Lab2_4_5.java
- * author: Laura Florez
- * course: CMPT 220
- * assignment: Lab2
- * due date: Sept 14/16
- * version: 1.0
- * 
- * The key concepts of this lab is the use of selections, mathematical functions, characters and strings.
- */
+* file: Lab2_4_5.java
+* author: Laura Florez
+* course: CMPT 220
+* assignment: Lab2
+* due date: Sept 14/16
+* version: 1.0
+* 
+* The key concepts of this lab is the use of selections, 
+* mathematical functions, characters and strings.
+*/
 
 import java.util.Scanner;
 
 public class Lab2_4_5{
   public static void main (String[] args) {
-  	//Propmt the user to enter a value
-  	Scanner input = new Scanner(System.in);
-  	System.out.print("Enter the number of sides and the length: ");
-  	double numberSides = input.nextDouble();
-  	double lengthSides = input.nextDouble();
+    //Propmt the user to enter a value
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter the number of sides and the length: ");
+    double numberSides = input.nextDouble();
+    double lengthSides = input.nextDouble();
 
-  	//Calculate area
-  	double area = (numberSides * (Math.pow( lengthSides, 2)) ) / (4 * (Math.tan(Math.PI/numberSides)));
+    //Calculate area
+    double area = (numberSides * (Math.pow( lengthSides, 2)) ) / 
+      (4 * (Math.tan(Math.PI/numberSides)));
 
-  	System.out.println("The area of the polygon is " + area);
+    System.out.println("The area of the polygon is " + area);
   }
 }
